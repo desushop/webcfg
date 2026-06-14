4918fca6bd44dcb49d9d5fd19bcffac92f5f43c5
maoperson <maoperson@pm.me>
2026-06-14 16:34:41 +0200

    implement serve function
    serve is the final setup function which starts the service lifecycle.

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -16,3 +16,20 @@
 //! - write function
 //! ## license
 //! © single license - AGPL-3.0 - maoperson@desushop
+
+use anyhow::Context;
+
+async fn serve<S>(socket: S, app: axum::Router) -> anyhow::Result<()>
+where S: tokio::net::ToSocketAddrs + std::fmt::Debug {
+    let listener = tokio::net::TcpListener::bind(&socket).await
+        .with_context(|| format!("failed to create socket from {socket:?}"))?;
+    axum::serve(listener, app).await
+        .with_context(|| "failed to start axum server")
+        .map(|_| ())
+}
+
+#[tokio::test]
+async fn test_serve() -> anyhow::Result<()> {
+    serve("::1", axum::Router::new()).await
+        .with_context(|| "failed to execute serve()")
+}
