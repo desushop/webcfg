0af89ad8ddc6a9c92e54e7839be8ba95f38f566c
maoperson <maoperson@pm.me>
2026-06-14 16:33:48 +0200

    provide documentation for main.rs

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,8 @@
+//! this module does not get tested,\
+//! and just combines functions from library,\
+//! providing a runnable default.
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    println!("Hello, webcfg!");
 }
