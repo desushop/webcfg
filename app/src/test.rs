134015133b4e1032886b60ac380bd17adfeca57a
maoperson <maoperson@pm.me>
2026-06-25 17:18:13 +0200

    update test_load_html

--- app/src/test.rs
+++ app/src/test.rs
@@ -19,7 +19,7 @@ async fn test_serve() -> anyhow::Result<()> {
 #[tokio::test]
 async fn test_load_html() -> anyhow::Result<()> {
     let mut router = axum::Router::new();
-    router.load_html(WORKING_DIR.deref().join("sites"))
-        .with_context(|| "ERR load html files");
+    let html = router.load_html(WORKING_DIR.deref().join("sites"))
+        .with_context(|| "ERR load html files")?;
     Ok(())
 }
