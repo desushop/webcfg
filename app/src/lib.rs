8db347d7cc9d0ae2096217d87a05d59a5b52d4b5
maoperson <maoperson@pm.me>
2026-06-25 17:17:41 +0200

    add throw_error to WebcfgRunnable
    throw_error will be used for appending errors which will get read by
    axum server at intervals.

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -44,4 +44,7 @@ pub(crate) trait WebcfgRunnable {
     type HtmlFile;
     /// loads html files from a target directory
     fn load_html<'a>(&mut self, target: impl AsRef<camino::Utf8Path>) -> anyhow::Result<&'a [Self::HtmlFile]>;
+
+    type Error;
+    fn throw_error(&mut self, error: impl Into<Self::   Error>);
 }
