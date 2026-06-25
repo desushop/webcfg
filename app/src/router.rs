ea172ad1620996289042222c0afc6ffb4ef15af3
maoperson <maoperson@pm.me>
2026-06-25 17:19:13 +0200

    add throw_error and work on load_html

--- app/src/router.rs
+++ app/src/router.rs
@@ -16,6 +16,26 @@ impl crate::WebcfgRunnable for axum::Router {
 
     type HtmlFile = ();
     fn load_html<'a>(&mut self, target: impl AsRef<camino::Utf8Path>) -> anyhow::Result<&'a [Self::HtmlFile]> {
+        let target = target.as_ref();
+        std::fs::read_dir(target)
+            .with_context(|| format!("ERR read directory {}", target))?
+            .filter_map(|r| match r {
+                Ok(d) => {
+                    if let Ok(path) = camino::Utf8PathBuf::from_path_buf(d.path()) {
+                        if let Ok(html) = compile_html::<Self::HtmlFile>(path) {
+                            return Some(html)
+                        } return None
+                    } return None
+                },
+                Err(e) => {
+                    self.throw_error(anyhow::anyhow!(e));
+                    return None
+                },
+            }); todo!("this fn is trash, do better and propagate")
+    } //TODO return iter
+
+    type Error = anyhow::Error;
+    fn throw_error(&mut self, error: impl Into<Self::Error>) {
         todo!()
     }
 }
