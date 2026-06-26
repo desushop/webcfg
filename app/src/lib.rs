d2743075fb75da76a00d067c341c0f2a8a76f2bd
maoperson <maoperson@pm.me>
2026-06-26 09:54:36 +0200

    add thiserror for toml error context

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -18,11 +18,13 @@
 //! © single license - AGPL-3.0 - maoperson@desushop
 
 pub use router::*;
+pub use err::*;
 
 use std::{fs, net, ops::Deref};
 use anyhow::Context;
 use std::fmt::Debug;
 
+mod err;
 mod router;
 #[cfg(test)]
 mod test;
@@ -68,19 +70,19 @@ pub trait WebcfgRunnable {
     fn throw_error(&mut self, error: impl Into<Self::Error>);
 }
 
-pub fn read_toml(target: impl AsRef<camino::Utf8Path>) -> anyhow::Result<Config> {
+pub fn read_toml(target: impl AsRef<camino::Utf8Path>) -> Result<Config, err::TomlError> {
     let target = target.as_ref();
     let bytes = std::fs::read(target)
-        .with_context(|| format!("ERR read file \"{}\"", &target))?;
+        .map_err(|e| err::TomlError::Read(target.to_path_buf(), format!(": {e}")))?;
     toml::from_slice(bytes.as_slice())
-        .with_context(|| format!("ERR deserialize toml \"{}\"", &target))
+        .map_err(|e| err::TomlError::Deserialize(target.to_path_buf(), format!(": {e}")))
 }
 
-pub fn write_toml<C>(contents: C, target: impl AsRef<camino::Utf8Path>) -> anyhow::Result<impl AsRef<camino::Utf8Path>>
+pub fn write_toml<C>(contents: C, target: impl AsRef<camino::Utf8Path>) -> Result<impl AsRef<camino::Utf8Path>, err::TomlError>
 where for<'a> C: 'a + serde::Serialize + Debug {
     let bytes = toml::to_string_pretty(&contents)
-        .with_context(|| format!("ERR serialize toml \"{contents:?}\""))?;
+        .map_err(|e| err::TomlError::Serialize(target.as_ref().to_path_buf(), format!(": {e}")))?;
     fs::write(&target.as_ref(), bytes)
-        .with_context(|| format!("ERR write file \"{}\"", &target.as_ref()))
+        .map_err(|e| err::TomlError::Write(target.as_ref().to_path_buf(), format!(": {e}")))
         .map(|_| target)
 }
