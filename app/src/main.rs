1203e576a4d1b55c3823c3d392946f4880ae3caf
maoperson <maoperson@pm.me>
2026-06-30 11:09:30 +0200

    update main to index html files once at runtime

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,16 +2,27 @@
 //! and just combines functions from library,\
 //! providing a runnable default.
 
+use std::collections;
 use anyhow::Context;
 use webcfg::*;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config = get_config().with_context(|| "ERR get config")?;
-
     let ip = std::net::Ipv6Addr::LOCALHOST;
     let socket = std::net::SocketAddrV6::new(ip, 35800, 0, 0);
-    axum::Router::new().serve(socket).await
+    let mut router = axum::Router::new();
+    let path = WORKING_DIR.join("sites");
+    let dir = path.read_dir_utf8().with_context(|| format!("ERR read directory {path}"))?;
+    let set = router.index_html(dir)
+        .filter_map(|e| match e {
+            Ok(p) => Some(p),
+            Err(e) => {
+                println!("{e:?}");
+                None
+            },
+        }).collect::<collections::HashSet<_>>();
+    router.serve(socket).await
 }
 
 fn get_config() -> anyhow::Result<webcfg::Config> {
