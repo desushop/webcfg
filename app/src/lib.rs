3f570c3b5f74178cfc77b7fae6bf7ad3e03bbebb
maoperson <maoperson@pm.me>
2026-07-07 09:31:07 +0200

    update html filter default

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -56,8 +56,8 @@ impl App {
 pub struct Config {
     ip: net::IpAddr,
     port: u16,
-    tags: ahash::HashSet<String>,
-    attr: ahash::HashMap<String, ahash::HashSet<String>>
+    tags: HashSet<String>,
+    attr: HashMap<String, HashSet<String>>
 }
 
 impl Default for Config {
@@ -66,21 +66,22 @@ impl Default for Config {
             "class", "draggable", "dir", "title", "role",
             "aria-checked", "aria-autocomplete", "aria-disabled", "aria-label", "aria-valuemax", "aria-valuemin", "aria-valuenow", "aria-valuetext", "aria-placeholder",
             "aria-dropeffect", "aria-dragged",
-            "aria-busy", "aria-description", "aria-details", "aria-label", "aria-keyshortcuts"];
+            "aria-busy", "aria-description", "aria-details", "aria-label", "aria-keyshortcuts", "value", "name"];
         Self {
             ip: net::IpAddr::V6(net::Ipv6Addr::LOCALHOST),
             port: 35800,
-            tags: ahash::HashSet::from_iter([
-                "div", "span", "style", "a", "body", "button", "br", "code", "details", "head", "footer", "html", "img", "input", "i", "p", "svg", "textarea", "title", "option"
+            tags: HashSet::from_iter([
+                "div", "span", "style", "a", "body", "button", "br", "code", "details", "head", "footer", "html", "img", "input", "i", "p", "svg", "textarea", "title", "option", "form"
             ].into_iter().map(String::from)),
-            attr: ahash::HashMap::from_iter([
-                ("img".to_owned(), ahash::HashSet::from_iter(["src", "href", "alt"].into_iter().chain(default_tags.clone()).map(String::from))),
-                ("input".to_owned(), ahash::HashSet::from_iter(["alt", "placeholder", "readonly", "required", "list", "disabled"].into_iter().chain(default_tags.clone()).map(String::from))),
-                ("html".to_owned(), ahash::HashSet::from_iter(["lang"].into_iter().map(String::from))),
-                ("a".to_owned(), ahash::HashSet::from_iter(["href"].into_iter().map(String::from))),
-                ("textarea".to_owned(), ahash::HashSet::from_iter(["wrap", "rows", "required", "readonly", "placeholder", "disabled"].into_iter().chain(default_tags.clone()).map(String::from))),
-                ("button".to_owned(), ahash::HashSet::from_iter(["disabled"].into_iter().chain(default_tags.clone()).map(String::from))),
-                ("option".to_owned(), ahash::HashSet::from_iter(["disabled"].into_iter().map(String::from))),
+            attr: HashMap::from_iter([
+                ("img".to_owned(), HashSet::from_iter(["src", "href", "alt"].into_iter().chain(default_tags.clone()).map(String::from))),
+                ("input".to_owned(), HashSet::from_iter(["alt", "placeholder", "readonly", "required", "list", "disabled", "type", "src"].into_iter().chain(default_tags.clone()).map(String::from))),
+                ("html".to_owned(), HashSet::from_iter(["lang"].into_iter().map(String::from))),
+                ("a".to_owned(), HashSet::from_iter(["href"].into_iter().map(String::from))),
+                ("textarea".to_owned(), HashSet::from_iter(["wrap", "rows", "required", "readonly", "placeholder", "disabled"].into_iter().chain(default_tags.clone()).map(String::from))),
+                ("button".to_owned(), HashSet::from_iter(["disabled"].into_iter().chain(default_tags.clone()).map(String::from))),
+                ("option".to_owned(), HashSet::from_iter(["disabled"].into_iter().map(String::from))),
+                ("form".to_owned(), HashSet::from_iter(["action", "method"].into_iter().map(String::from))),
             ].into_iter())
         }
     }
